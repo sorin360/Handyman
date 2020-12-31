f0deeb61a97fd93e57c87fce48e923866d8d187d
slica <sorin.lica@atoss.com>
2020-12-31 12:54:13 +0200

    Initial Commit

---
+++ Handyman/HandymanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandymanApp.swift
+//  Handyman
+//
+//  Created by Lica Sorin on 31/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct HandymanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
